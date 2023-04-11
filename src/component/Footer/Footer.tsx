fc3d5d0cec56f2525301a5e089dbcdd22a742c50
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-04-11 17:01:17 +0300

    with footer and lobbypage

---
+++ src/component/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+const Footer: React.FC = () => {
+	return (
+		<footer id="footerPage">
+			<hr className="footer-hr" />
+			<div className="footer-info">
+				<div id="info"> Term of Use</div>
+				<div id="info">Privacy Policy</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
--- src/component/LobbyPage/LobbyPage.tsx
+++ src/component/LobbyPage/LobbyPage.tsx
@@ -2,13 +2,9 @@ import React from 'react';
 
 const LobbyPage: React.FC = () => {
 	return (
-		<footer id="footerPage">
-			<hr className="footer-hr" />
-			<div className="footer-info">
-				<div id="info"> Term of Use</div>
-				<div id="info">Privacy Policy</div>
-			</div>
-		</footer>
+		<div>
+			<h1>Choose Code Block</h1>
+		</div>
 	);
 };
 
