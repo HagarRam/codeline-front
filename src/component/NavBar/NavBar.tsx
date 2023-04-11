82ce3280d382ccfa73e603c0ea777b2deb95abfe
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-04-11 16:59:25 +0300

    with lobby and navbar

---
+++ src/component/NavBar/NavBar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar: React.FC = () => {
+	return (
+		<footer id="footerPage">
+			<hr className="footer-hr" />
+			<div className="footer-info">
+				<div id="info"> Term of Use</div>
+				<div id="info">Privacy Policy</div>
+			</div>
+		</footer>
+	);
+};
+
+export default NavBar;
