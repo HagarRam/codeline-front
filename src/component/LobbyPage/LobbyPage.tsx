979a34730d9c3f3ae9d54a9bf79a62d4f0d932ed
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-04-11 17:04:22 +0300

    with apps

--- src/component/LobbyPage/LobbyPage.tsx
+++ src/component/LobbyPage/LobbyPage.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from '../Footer/Footer';
+import NavBar from '../NavBar/NavBar';
 
 const LobbyPage: React.FC = () => {
 	return (
 		<div>
+			<NavBar />
 			<h1>Choose Code Block</h1>
+			<Footer />
 		</div>
 	);
 };
--- src/component/NavBar/NavBar.tsx
+++ src/component/NavBar/NavBar.tsx
@@ -1,15 +1,7 @@
 import React from 'react';
 
 const NavBar: React.FC = () => {
-	return (
-		<footer id="footerPage">
-			<hr className="footer-hr" />
-			<div className="footer-info">
-				<div id="info"> Term of Use</div>
-				<div id="info">Privacy Policy</div>
-			</div>
-		</footer>
-	);
+	return <div>CodeLine</div>;
 };
 
 export default NavBar;
