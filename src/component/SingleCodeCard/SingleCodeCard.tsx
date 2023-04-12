bb762e14ea5d33037bd40474f46fc9eb0c4405dc
HagarRam <119937814+HagarRam@users.noreply.github.com>
2023-04-12 13:34:42 +0300

    with socket

--- src/component/SingleCodeCard/SingleCodeCard.tsx
+++ src/component/SingleCodeCard/SingleCodeCard.tsx
@@ -1,15 +1,52 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { ICodeBlock } from '../../store/slices/codeDatasSlice';
 import './SingleCodeCard.css';
+import io from 'socket.io-client';
+
 const SingleCodeCard: React.FC<ICodeBlock> = (props: ICodeBlock) => {
+	const { title, code } = props;
 	const [SubmitModal, setSubmitModal] = useState(false);
+	const [socket, setSocket] = useState<any>(null);
+	const [codeBlock, setCodeBlock] = useState('');
+
+	useEffect(() => {
+		// Connect to Socket.IO server
+		const socket = io('http://localhost:7000');
+
+		socket.on('connect', () => {
+			// Join the "code room" and emit the client's identifier
+			socket.emit('join', 'code room');
+			setSocket(socket);
+		});
+
+		socket.on('code block', (data: any) => {
+			// Receive code block from server and update state
+			setCodeBlock(data.code);
+		});
+
+		return () => {
+			// Disconnect from Socket.IO server on component unmount
+			socket.disconnect();
+		};
+	}, []);
+
 	const handelSubmit = () => {
+		// Emit code block to server
+		socket.emit('code block', { code: codeBlock });
 		setSubmitModal(true);
 	};
+
 	const handelCancel = () => {
 		setSubmitModal(false);
 	};
-	const { title, code } = props;
+
+	const handelCodeBlockChange = (
+		event: React.ChangeEvent<HTMLTextAreaElement>
+	) => {
+		// Update local state with code block
+		setCodeBlock(event.target.value);
+	};
+
 	return (
 		<div className="subject-information">
 			<div id="card-information">
@@ -19,13 +56,15 @@ const SingleCodeCard: React.FC<ICodeBlock> = (props: ICodeBlock) => {
 				<textarea
 					id="text-area"
 					placeholder="enter your code...."
+					value={codeBlock}
+					onChange={handelCodeBlockChange}
 				/>
 				<div id="code-container">{code}</div>
 			</div>
 
 			<div
 				id="submit-button"
-				onClick={() => handelSubmit()}>
+				onClick={handelSubmit}>
 				SUBMIT
 			</div>
 			{SubmitModal && (
